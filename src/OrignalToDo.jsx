aa283be4d545aa0804c9ba8da38f94110105330d
sumittak119 <sumittak119@gmail.com>
2025-04-10 16:48:32 +0530

    To-Do Update with new App

--- src/OrignalToDo.jsx
+++ src/OrignalToDo.jsx
@@ -2,14 +2,14 @@
 import React, { useState } from 'react'
 import './App.css'
 
+function OrignalToDo() { //Create a component in which all code written
 
-function OrignalToDo() {
   const [todo, setTodo] = useState("")
   const [todos, setTodos] = useState([])
   const [editId, setEditId] = useState(null)
   const [editValue, setEditValue] = useState("")
   
-  function handleAdd() {
+  function handleAdd() { //This function handle task addition in array 
     if (todo.trim() !== "") {
       const newTodo = {
         id: Date.now(),
@@ -18,13 +18,14 @@ function OrignalToDo() {
       setTodos([...todos, newTodo])
       setTodo("")
     }
-    
   }
-  function handleEdit(id, text) {
+
+  function handleEdit(id, text) { //This function handle task edit functionality
     setEditId(id)
     setEditValue(text)
   }
-  function handleSaveEdit(id) {
+
+  function handleSaveEdit(id) { //This function handle save and edit functionality
     const updatedTodos = todos.map(item => {
       if (item.id === id) {
         return { ...item, text: editValue }
@@ -36,7 +37,7 @@ function OrignalToDo() {
     setEditId(null)
   }
 
-  function handleDelete(id) {
+  function handleDelete(id) { //This function handle deleting a task functionality
     const filteredTodos = todos.filter(item => item.id !== id)
     setTodos(filteredTodos)
   }
@@ -45,14 +46,13 @@ function OrignalToDo() {
       handleAdd()
     }
   }
-
   return (
     <>
       <div>
-      <div  className="bg-gradient-to-b from-black to-white">
-      <div className='container mx-auto  rounded-xl p-5 bg-violet-200 min-h-screen'>
+      <div className="bg-gradient-to-b from-[#956d97] to-[#2580B3]">
+      <div className='container mx-auto w-auto rounded-xl bg-gradient-to-b from-[#ad8c8c] to-[#386eed] text-xl p-5 min-h-screen text-center'>
           <div className='addtodo'>
-            <h2 className='text-xl font-bold my-5'>Add a ToDo</h2>
+            <h2 className='text-2xl font-bold my-5'>Add a ToDo</h2>
             <input 
               type='text' 
               value={todo}
@@ -61,6 +61,8 @@ function OrignalToDo() {
               placeholder='Enter Task' 
               className='border rounded-md mx-5 bg-white w-80 p-1'
             />
+            <br/><br/>
+            <p/>
             <button 
               onClick={handleAdd} 
               className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-white text-sm font-bold rounded-md'>
@@ -72,7 +74,7 @@ function OrignalToDo() {
           <div className='todos'>
              {(
               todos.map(item => (
-                <div key={item.id} className='todo flex justify-between items-center bg-white p-2 my-2 rounded-md'>
+                <div key={item.id} className='todo flex justify-between text-left bg-white p-2 my-2 rounded-md'>
                   {editId === item.id ? (
                     <div className="edit-form flex-grow flex">
                       <input
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
+import App from './App.jsx'
 import OrignalToDo from './OrignalToDo.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <OrignalToDo />
+    <App />
   </StrictMode>,
 )
